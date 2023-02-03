4b139f47bbf8edc2ddb3328edc661a17db889406
Killian Schneider <80042061+yoschigamer@users.noreply.github.com>
2023-02-03 18:04:48 +0100

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-let value = "https://i.imgur.com/qpvUY0C.gif"; // Défault Slider
+let value = "https://media.tenor.com/WtDDOBNCKB8AAAAi/sonic-running.gif"; // Défault Slider
 
 chrome.runtime.onMessage.addListener(function (response, sender, sendResponse) {
   value = response
@@ -19,16 +19,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, info) => {
         func: SliderFunction,
         args: [value]
       }
-    ).then(() => { console.log('script injected') });
-  }
-})
-
-chrome.tabs.onUpdated.addListener(async (tabId, info) => {
-  if (info.status == "complete") {
-
-    const query = { active: true, lastFocusedWindow: true }
-    const result = await chrome.tabs.query(query)
-
+    ).then(() => { console.log('script SliderFunction injected') });
     chrome.scripting.executeScript(
       {
         target: {
@@ -36,27 +27,21 @@ chrome.tabs.onUpdated.addListener(async (tabId, info) => {
         },
         func: fullscreen,
       }
-    ).then(() => { console.log('script injected') });
+    ).then(() => { console.log('script fullscreen injected') });
   }
 })
 
 function fullscreen() {
-  element = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-right-controls > button.ytp-fullscreen-button.ytp-button")
-  var oldTitle = element.title;
-
-  function myFunc(Element) {
-    if (Element.title == 'Plein écran (f)')
-      document.querySelector("#page-manager > ytd-watch-flexy").style.marginTop = "50px";
-    else
-      document.querySelector("#page-manager > ytd-watch-flexy").style.marginTop = "0px";
-    window.scrollTo(0, 0);
-  }
+  let fullscreen = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-right-controls > button.ytp-fullscreen-button.ytp-button")
+  var oldTitle = fullscreen.title;
 
   window.setInterval(function () {
-    if (element.title !== oldTitle) {
-      myFunc(element);
+    if (fullscreen.title !== oldTitle) {
+      (fullscreen.title == 'Plein écran (f)')
+        ? (document.querySelector("#page-manager > ytd-watch-flexy").style.marginTop = "50px")
+        : (document.querySelector("#page-manager > ytd-watch-flexy").style.marginTop = "0px", window.scrollTo(0, 0));
     }
-    oldTitle = element.title;
+    oldTitle = fullscreen.title;
   }, 100); //check every 100ms
 }
 
@@ -64,21 +49,22 @@ function SliderFunction(value) {
   function styled() {
     var newDivStyle = document.createElement('style');
     newDivStyle.innerHTML = `
-          :root{
-              --size: 50px !important
-          }
-  
-          #img-container {
-              background: url("${value}") 0% 0% / contain no-repeat;
-              top: calc(var(--size)*-1) !important;
-              left: calc(var(--size)/2*-1) !important;
-              width: var(--size);
-              height: var(--size); 
-              position: relative;
-          }   
-          `;
+      :root{
+        --size: 50px !important
+      }
+
+      #img-container {
+        background: url("${value}") 0% 0% / contain no-repeat;
+        top: calc(var(--size)*-1.2) !important;
+        left: calc(var(--size)/2*-1) !important;  
+        width: var(--size);
+        height: var(--size); 
+        position: relative;
+      }
+    `;
     document.head.appendChild(newDivStyle);
   } // StyleSheet
+
   if (!(document.querySelector("#img-container") != null)) {
     const currentDiv = document.querySelector("#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar > div.ytp-scrubber-container")
     // create a new div element
@@ -87,7 +73,10 @@ function SliderFunction(value) {
 
     styled() // Css du slider
 
-    // Create a new div
+    // liée la div à l'element
     currentDiv.appendChild(newDiv);
+  }
+  else {
+    return;
   }
 }
